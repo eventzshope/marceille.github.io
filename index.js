5448b7d45408af3a44565339735d8baca8ae7e09
eventzshope <98055568+eventzshope@users.noreply.github.com>
2022-01-22 18:57:26 -0500

    Update index.js

--- index.js
+++ index.js
@@ -1,3 +1,106 @@
+//event de click pour le menu
+var btn = document.getElementById("click_img");
+var menues = document.getElementById("menues");
+
+var coeurvideclick = document.getElementById("coeurvide");
+var coeurfull  = document.getElementById("coeurfull");
+
+var coeurvideclick1 = document.getElementById("coeurvide1");
+var coeurfull1 = document.getElementById("coeurfull1");
+
+var coeurvideclick2 = document.getElementById("coeurvide2");
+var coeurfull2 = document.getElementById("coeurfull2");
+
+var coeurvideclick3 = document.getElementById("coeurvide3");
+var coeurfull3 = document.getElementById("coeurfull3");
+
+
+var coeurvideclick4 = document.getElementById("coeurvide4");
+var coeurfull4 = document.getElementById("coeurfull4");
+
+
+var coeurvideclick5 = document.getElementById("coeurvide5");
+var coeurfull5 = document.getElementById("coeurfull5");
+
+
+btn.addEventListener('click', vues);
+
+function vues() {
+  menues.classList.toggle('vue');
+}
+
+//fin
+
+
+
+coeurvideclick.addEventListener('click',a);
+function a(){
+  coeurfull.classList.add('nn');
+  
+}
+coeurfull.addEventListener('click',a1);
+function a1(){
+ coeurfull.classList.remove('nn')
+  
+}
+
+
+coeurvideclick1.addEventListener('click',b);
+function b(){
+  coeurfull1.classList.add('nn');
+  
+}
+coeurfull1.addEventListener('click',b1);
+function b1(){
+ coeurfull1.classList.remove('nn')
+  
+}
+
+coeurvideclick2.addEventListener('click',c);
+function c(){
+  coeurfull2.classList.add('nn');
+  
+}
+coeurfull2.addEventListener('click',c1);
+function c1(){
+ coeurfull2.classList.remove('nn')
+  
+}
+coeurvideclick3.addEventListener('click',d);
+function d(){
+  coeurfull3.classList.add('nn');
+  
+}
+coeurfull3.addEventListener('click',d1);
+function d1(){
+ coeurfull3.classList.remove('nn')
+  
+}
+
+coeurvideclick4.addEventListener('click',e);
+function e(){
+  coeurfull4.classList.add('nn');
+  
+}
+coeurfull4.addEventListener('click',e1);
+function e1(){
+ coeurfull4.classList.remove('nn')
+  
+}
+
+coeurvideclick5.addEventListener('click',f);
+function f(){
+  coeurfull5.classList.add('nn');
+  
+}
+coeurfull5.addEventListener('click',f1);
+function f1(){
+ coeurfull5.classList.remove('nn')
+  
+}
+
+
+
 window.addEventListener("DOMContentLoaded", function () {
   // get the form elements defined in your form HTML above
 
